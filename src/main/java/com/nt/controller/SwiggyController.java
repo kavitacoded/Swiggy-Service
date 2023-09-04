e5d7bfc885600d22f27c13956e46955692175bd7
unknown <kavi20900@gmail.com>
2023-09-04 15:25:58 +0530

    order from swiggys

--- src/main/java/com/nt/controller/SwiggyController.java
+++ src/main/java/com/nt/controller/SwiggyController.java
@@ -24,7 +24,7 @@ public class SwiggyController {
 	
 	@GetMapping("/details")
 	public ResponseEntity<?>showOrders(){
-		return new ResponseEntity<String>(user + "from custormers",HttpStatus.OK);
+		return new ResponseEntity<String>(user + "from custormers....",HttpStatus.OK);
 	}
 	
 	
